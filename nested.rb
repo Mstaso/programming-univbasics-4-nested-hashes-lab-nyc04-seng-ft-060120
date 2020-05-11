4f34f766cd0ea738f3de518527b65d9bc2dd0a1f
malcolm staso <mstaso1@gmail.com>
2020-05-11 18:27:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,9 +106,8 @@ def changing_alan
         :known_for => "Unix",
         :languages => ["C"]
       }
-      programmer_hash[:alan_kay][:known_for] = "GUI"
     }
-    programmer_hash
+    programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
